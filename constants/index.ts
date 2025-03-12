339f35fbb52fb965cfcb964aab42ed75fc4f4754
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-03-12 08:27:02 +0000

    field types and names used in authform

---
+++ constants/index.ts
@@ -0,0 +1,13 @@
+export const FIELD_NAMES = {
+    username: "Full name",
+    email: "Email",
+    password: "Password",
+    otp: "OTP",
+  };
+  
+  export const FIELD_TYPES = {
+    username: "text",
+    email: "email",
+    password: "password",
+    otp: "number"
+  };
