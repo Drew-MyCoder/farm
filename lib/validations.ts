0a8950d54e977082221be4a1262847190f1cc74c
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-03-12 08:27:36 +0000

    zod validation for signin and sign up

---
+++ lib/validations.ts
@@ -0,0 +1,21 @@
+// "use server"
+
+import { z } from "zod"
+
+
+export const signUpSchema = z.object({
+    username: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(4),
+});
+
+
+export const signInSchema = z.object({
+    username: z.string().min(3),
+    password: z.string().min(4),
+})
+
+export const verifyOTP = z.object({
+    username: z.string().min(3),
+    otp: z.string().max(6),
+})
