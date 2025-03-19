df5480a0f11584a347f09fe945e95ab33f5b1f1a
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-03-19 19:17:53 +0000

    created getusername and getuserid fxns

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,52 @@
 import { clsx, type ClassValue } from "clsx"
+import { useEffect, useState } from "react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+
+export function getUsername() {
+
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    // only run on client side
+    if (typeof window !== 'undefined') {
+      let storedName = localStorage.getItem('userName');
+
+      if (!storedName) {
+        storedName = localStorage.getItem('user')
+      }
+      setUserName(storedName);
+    }
+  }, []);
+
+  console.log(userName, 'this is my username')
+
+  return userName;
+}
+
+
+export function getUserId() {
+
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    // only run on client side
+    if (typeof window !== 'undefined') {
+      let storedName = localStorage.getItem('userId');
+
+      if (!storedName) {
+        storedName = localStorage.getItem('user_id')
+      }
+      setUserId(storedName);
+    }
+  }, []);
+
+  console.log(userId, 'this is my userId')
+
+  return userId;
 }
