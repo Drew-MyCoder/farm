8770fe4c882d3206bee28b0f7f5b0a996c795f5f
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-03-25 04:47:14 +0000

    changed from google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,23 @@
 import type React from "react"
-import { Inter } from "next/font/google"
+import localFont from "next/font/local";
 import "./globals.css"
 
 
-const inter = Inter({ subsets: ["latin"] })
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const bebasNeue = localFont({
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
 
 export const metadata = {
   title: "Worker Portal",
@@ -17,7 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}>{children}
       {/* <Toaster /> */}
       </body>
     </html>
