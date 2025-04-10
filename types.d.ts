bacaa2b0bf60402ffea8b48fd1fcd4cab89e63d7
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-04-10 20:34:23 +0000

    types for coops and orders

--- types.d.ts
+++ types.d.ts
@@ -4,3 +4,48 @@ interface AuthCredentials {
     email: string;
     password: string;
   }
+
+
+  type CoopData = {
+    id: number;
+    parent_id: number | null;
+    status: string;
+    total_dead_fowls: number;
+    total_fowls: number;
+    coop_name: string;
+    total_feed: number;
+    created_at: string;
+    updated_at: string;
+    egg_count: number;
+    efficiency: number;
+    collection_date: string;
+    broken_eggs: number;
+    notes: string;
+  };
+
+  
+  type DailyGroup = {
+    date: string;
+    coops: CoopEntry[];
+  };
+
+
+  type CoopEntry = {
+    id: number;
+    coop_name: string;
+    egg_count: number;
+    // Add any other properties as necessary
+  };
+
+
+  type Order = {
+  id: number
+  name: string
+  date_of_delivery: string
+  crates_desired: number
+  amount: number
+  status_of_delivery: "pending" | "processing" | "delivered" | "cancelled"
+  created_at: string
+  updated_at: string
+  by: string
+  }
